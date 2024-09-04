4b5a43dcf13c627f05adad21378f45fafa277554
우준 안 <enos96@naver.com>
2024-09-04 19:19:19 +0900

    12

--- 001. 02-10 제어구조와 배열 109p (LAB 문제 Tic-Tac-Toe 게임)/소스.cpp
+++ 001. 02-10 제어구조와 배열 109p (LAB 문제 Tic-Tac-Toe 게임)/소스.cpp
@@ -6,6 +6,19 @@ using namespace std;
 
 int main()
 {
+	int x, y;
+	char arr[3][3] = {};
+
+	for (int i = 0; i < 3; ++i)
+	{
+		for (int j = 0; j < 3; ++j)
+		{
+			arr[i][j] = ' ';
+		}
+	}
+
+	cout << "(" << "x" << "," << "y" << ")" << " " << "ÁÂÇ¥: ";
+	cin >> x >> y;
 
 
 
